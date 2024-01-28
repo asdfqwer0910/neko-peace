376109901a026b396d2e43eded73df880820c04f
asdfqwer0910 <2101385@s.asojuku.ac.jp>
2024-01-29 06:46:23 +0900

    commit

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -12,8 +12,8 @@ type ProfileType = Database['public']['Tables']['profiles']['Row']
 
 const NavBar = ({
   session, profile
-}: { 
-  session: Session | null 
+}: {
+  session: Session | null
   profile: ProfileType | null
 }) => {
   const { setUser } = useStore()
@@ -30,70 +30,70 @@ const NavBar = ({
     })
   }, [session, setUser, profile])
 
-  const [ isOpen, setIsOpen ] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
   console.log(isOpen)
 
   return (
-  <nav className="flexBetween px-6 lg:px-20 xl:px-20 relative z-30 py-5 h-[90px] shadow">
-    <div className="relative w-32 h-full">
-      <Link href="/">
-        <Image src="/Logo.svg" alt="logo" fill />
-      </Link>
-    </div>
-
-    <ul className="hidden h-full gap-12 lg:flex">
-      {NAV_LINKS.map((link) => (
-        <Link href={link.href} key={link.key} className="text-lg font-bold text-gray-500 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold hover:text-orange-400 hover:text-xl">
-          {link.label}
+    <nav className="flexBetween px-6 lg:px-20 xl:px-20 relative z-30 py-5 h-[90px] shadow">
+      <div className="relative w-32 h-full">
+        <Link href="/">
+          <Image src="/Logo.svg" alt="logo" fill />
         </Link>
-      ))}
-    </ul>
-    
-    {session ?
-    <div className="lg:flexCenter hidden">
-      <button onClick={() => setIsOpen((prev) => !prev)}>
-        <div className="relative w-12 h-12">
-          <Image 
-            src={profile && profile.avatar_url ? profile.avatar_url : '/default.svg'}
-            className="border-2 rounded-full object-cover"
-            alt="avatar"
-            fill
+      </div>
+
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link href={link.href} key={link.key} className="text-lg font-bold text-gray-500 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold hover:text-orange-400 hover:text-xl">
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+
+      {session ?
+        <div className="lg:flexCenter hidden">
+          <button onClick={() => setIsOpen((prev) => !prev)}>
+            <div className="relative w-12 h-12">
+              <Image
+                src={profile && profile.avatar_url ? profile.avatar_url : '/default.svg'}
+                className="border-2 rounded-full object-cover"
+                alt="avatar"
+                fill
+              />
+            </div>
+          </button>
+
+          {isOpen && (
+            <div className='absolute top-20 flex flex-col py-2 border rounded bg-white animate-scale-up-ver-top'>
+              {DROPDOWN.map((link) => (
+                <Link href={link.href} key={link.href} className="py-2 px-3 font-semibold hover:bg-blue-600 hover:text-white">
+                  <Image
+                    src={link.image}
+                    alt="icon"
+                    width={23}
+                    height={23}
+                    className="inline-block mr-2"
+                  />
+                  {link.label}
+                </Link>
+              ))}
+            </div>
+          )}
+
+          <ul className="md:hidden bg-white absolute w-full h-full bottom-0">
+
+          </ul>
+        </div>
+        :
+        <div className="lg:flexCenter hidden">
+          <Button
+            type="button"
+            title="ログイン"
+            icon="/User.svg"
+            variant="btn_dark_green"
           />
         </div>
-      </button>
-
-      {isOpen && (
-        <div className='absolute top-20 flex flex-col py-2 border rounded bg-white animate-scale-up-ver-top'>
-          {DROPDOWN.map((link) => (
-            <Link href={link.href} className="py-2 px-3 font-semibold hover:bg-blue-600 hover:text-white">
-                <Image 
-                  src={link.image}
-                  alt="icon"
-                  width={23}
-                  height={23}
-                  className="inline-block mr-2"
-                />
-                {link.label}
-            </Link>
-          ))}
-        </div>
-      )}
-
-      <ul className="md:hidden bg-white absolute w-full h-full bottom-0">
-
-      </ul>
-    </div>
-    :
-    <div className="lg:flexCenter hidden">
-      <Button
-        type="button"
-        title="ログイン"
-        icon="/User.svg"
-        variant="btn_dark_green"
-      />
-    </div>
-    }
-  </nav>
+      }
+    </nav>
   )
 }
 
